ab1fd41f5e1501a25ad418befdd9512aa7118339
Arathain <arathainfarqoe@gmail.com>
2022-11-24 22:19:22 +0100

    soop x2

--- src/main/java/arathain/amogus/mixin/LivingEntityMixin.java
+++ src/main/java/arathain/amogus/mixin/LivingEntityMixin.java
@@ -5,17 +5,27 @@ import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.EntityDamageSource;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.UUID;
+
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Shadow public abstract void readCustomDataFromNbt(NbtCompound nbt);
+
     public LivingEntityMixin(EntityType<?> entityType, World world) {
         super(entityType, world);
     }
@@ -28,6 +38,26 @@ public abstract class LivingEntityMixin extends Entity {
     private void eplayers$canTarget(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
         if(entity instanceof PlayerEntity player &&  EliminatePlayers.bannedUuids.contains(player.getUuid()))
             cir.setReturnValue(false);
+    }
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void eplayers$fuckOffDiansuTrademark(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if(source instanceof EntityDamageSource e && e.getAttacker() instanceof PlayerEntity player && EliminatePlayers.bannedUuids.contains(player.getUuid()) && Registry.ENTITY_TYPE.getId(this.getType()).getPath().contains("soulmould")) {
+            NbtCompound nbt = new NbtCompound();
+            this.writeCustomDataToNbt(nbt);
+            UUID owner = null;
+            if(nbt.contains("Owner")) {
+                nbt.remove("ActionState");
+                nbt.putInt("ActionState", 1);
+                owner = nbt.getUuid("Owner");
+                nbt.remove("Owner");
+            }
+            nbt.putUuid("Owner", player.getUuid());
+            this.readCustomDataFromNbt(nbt);
+            if(this.world.getPlayerByUuid(owner) != null && ((LivingEntity)(Object)this) instanceof HostileEntity h) {
+                h.setTarget(this.world.getPlayerByUuid(owner));
+            }
+
+        }
     }
 
     @Override
